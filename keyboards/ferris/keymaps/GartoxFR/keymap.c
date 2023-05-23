db4bbf79ce3ead3694179870726bcf9e3dea86aa
Gartox <ewan71230@gmail.com>
2023-05-23 10:28:17 +0200

    Invert thumb button

--- keyboards/ferris/keymaps/GartoxFR/keymap.c
+++ keyboards/ferris/keymaps/GartoxFR/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T, /*|*/ KC_Y, KC_U,         KC_I,         KC_O,         KC_P,
         LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, /*|*/ KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),
         KC_Z,         KC_X,         KC_C,         KC_V,         KC_B, /*|*/ KC_N, KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
-              MO(1), KC_TAB, KC_SPC, MO(2)
+              KC_TAB, MO(1), MO(2), KC_SPC
     ),
     [1] = LAYOUT_split_3x5_2(
         /*
@@ -19,8 +19,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          */
         KC_ESC,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          /*|*/           KC_NO, KC_7, KC_8, KC_9, KC_BSPC,
         KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_ESC,          /*|*/           KC_BSPC, KC_4, KC_5, KC_6, KC_ENTER,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          /*|*/           KC_0,  KC_1, KC_2, KC_3, KC_DEL,
-                                    KC_NO, KC_TAB,          /*|*/           KC_SPC, MO(3)
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TAB,          /*|*/           KC_0,  KC_1, KC_2, KC_3, KC_DEL,
+                                    KC_NO, KC_NO,          /*|*/             MO(3), KC_SPC
     ),
     [2] = LAYOUT_split_3x5_2(
         /*
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RSFT(KC_GRV), KC_GRV, RSFT(KC_BACKSLASH), RSFT(KC_MINUS), KC_SLASH,
                         /*|*/ KC_BACKSLASH, RSFT(KC_2), RSFT(KC_3), RSFT(KC_1), RSFT(KC_SLASH),
 
-                           MO(3), KC_TAB,          /*|*/           KC_SPC, KC_NO
+                           KC_TAB, MO(3),          /*|*/           KC_NO, KC_NO
     ),
 
     [3] = LAYOUT_split_3x5_2(
