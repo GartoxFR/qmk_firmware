63de3baa775d3a15835cf15a2cae9980ded237e0
Gartox <ewan71230@gmail.com>
2024-02-05 22:33:00 +0100

    modifs

--- keyboards/ferris/keymaps/GartoxFR/keymap.c
+++ keyboards/ferris/keymaps/GartoxFR/keymap.c
@@ -17,7 +17,10 @@ enum Layers {
     SPC,
     LOL,
     LOL_FN,
-    GAME
+    GAME,
+    FACTORIO,
+    FACTORIO_FN,
+    OSU,
 };
 
 
@@ -79,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          */
         QK_BOOT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        /*|*/          KC_MEDIA_STOP, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, KC_NO,
         KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_ESC,         /*|*/          KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO,
-        KC_NO,   KC_NO,   DF(GAME),   DF(LOL),   KC_NO,          /*|*/          KC_NO, KC_BRID, KC_BRIU, KC_NO, KC_NO,
+        DF(OSU),   DF(FACTORIO),   DF(GAME),   DF(LOL),   KC_NO,          /*|*/          KC_NO, KC_BRID, KC_BRIU, KC_NO, KC_NO,
                                     KC_NO, KC_NO,          /*|*/           KC_NO, KC_NO
     ),
         [SPC] = LAYOUT_split_3x5_2(
@@ -115,14 +118,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,          /*|*/          KC_C, KC_Q, KC_S, KC_E, KC_LEFT_CTRL,
                                     XXXXXXX, DF(BASE),          /*|*/           KC_F, KC_SPC
     ),
-
+        [FACTORIO] = LAYOUT_split_3x5_2(
+        /*
+         * Media/Config layer
+         */
+        KC_ESC,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_T,        /*|*/          KC_1,  KC_2, KC_3, KC_4, KC_5,
+        KC_ENTER, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         /*|*/          KC_R, KC_A, KC_W, KC_D, KC_RIGHT_SHIFT,
+        XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,          /*|*/          KC_C, KC_Q, KC_S, KC_E, KC_LEFT_CTRL,
+                                    KC_TAB, XXXXXXX,          /*|*/           MO(FACTORIO_FN), KC_SPC
+    ),
+        [FACTORIO_FN] = LAYOUT_split_3x5_2(
+        /*
+         * Media/Config layer
+         */
+        KC_ESC,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_T,        /*|*/          KC_6,  KC_7, KC_8, KC_9, KC_0,
+        KC_ENTER, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         /*|*/          LCTL(KC_X), _______, _______, _______, KC_RIGHT_SHIFT,
+        XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,          /*|*/          KC_T, LCTL(KC_C), LCTL(KC_V), KC_B, LCTL(KC_Z),
+                                    KC_TAB, DF(BASE),          /*|*/           XXXXXXX, KC_SPC
+    ),
+        [OSU] = LAYOUT_split_3x5_2(
+            /*
+             * Osu!
+             */
+            KC_ESC,         KC_W,         KC_E,         KC_R,         KC_T, /*|*/ KC_F1, KC_F2,         KC_F3,         KC_F4,         KC_F5,
+            KC_A, KC_S, KC_D, KC_F, KC_G, /*|*/ KC_H, KC_J, KC_K, KC_L, KC_SCLN,
+            KC_Z,         KC_X,         KC_C,         KC_V,         KC_B, /*|*/ KC_N, KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
+                  KC_TAB, DF(BASE), DF(BASE), KC_SPC
+        ),
 
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, NUM, SYM, NAV);
 }
-
-
-
 
